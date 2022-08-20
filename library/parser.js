ee1c194fa4b38e990891a3a0f8f193d8c900cfcd
Banyapon Duangnoom <53414787+ohm002@users.noreply.github.com>
2022-08-20 20:03:04 +0700

    yea

--- library/parser.js
+++ library/parser.js
@@ -1,20 +1,33 @@
-function parse(file) {
+function parse(file, offset) {
     let array = file.split("\n");
     let notelist = [];
+    let notelistt = [];
+    let notelistc = [];
     let depth = 0;
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
         if (element.startsWith("t")) {
-            notelist.push({
+            notelistt.push({
                 "line": depth,
-                "ty" : (element.split(",")[0]), 
-                "st" : parseInt(element.split(",")[1]),
-                "pos" : parseInt(element.split(",")[2]), 
-                "hit" : false
-            })
+                "ty": (element.split(",")[0]),
+                "st": parseInt(element.split(",")[1]) + offset,
+                "pos": parseInt(element.split(",")[2]),
+                "hit": false
+            });
+        }
+        if (element.startsWith("c")) {
+            notelistc.push({
+                "line": depth,
+                "ty": (element.split(",")[0]),
+                "st": parseInt(element.split(",")[1]) + offset,
+                "pos": parseInt(element.split(",")[2]),
+                "hit": false
+            });
         }
         depth++;
     }
+    notelist.push(notelistt);
+    notelist.push(notelistc)
     console.log(notelist);
     return notelist;
 }
