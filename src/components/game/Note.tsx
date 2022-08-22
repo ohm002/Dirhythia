43612f07a55d36f67e22df51c29f67ead301f95f
Nanachi <dinhvu2509@gmail.com>
2022-08-22 23:45:10 +0700

    remove comment

--- src/components/game/Note.tsx
+++ src/components/game/Note.tsx
@@ -4,5 +4,4 @@ import { Texture } from 'pixi.js'
 
 export default function Note(n: any) {
 	return <Sprite texture={Texture.WHITE} x={400} y={n['n']} anchor={0.5} width={100} height={20}></Sprite>
-	// return <div>Note</div>
 }
