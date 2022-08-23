a8b562fd08343ab80d98cf7a8107e514c371d002
Ohm <53414787+ohm002@users.noreply.github.com>
2022-08-23 10:15:20 +0700

    comments

--- src/components/game/Column.tsx
+++ src/components/game/Column.tsx
@@ -10,6 +10,9 @@ type ColumnProps = {
 
 export default function Column(props: ColumnProps) {
   // todo handle col 1 keydown, check if it match the note start time
+  // for hold note : check if the keydown match the note starttime,
+  //                 check every certain milisec(possibly bpm or configured value) if there the hold note is still click (release for a short time will not trigger a miss)
+  //                 release timing is not fun to play
   return (
     <div>Column</div>
   )
